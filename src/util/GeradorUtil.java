1cf3626635a856c63d6d239ce987d4adc22875c0
gabriel.oening <gabriel.oening@PLE111D12.alunos.sc.senac.br>
2021-08-02 21:55:51 -0300

    gerador senha e cpf

--- src/util/GeradorUtil.java
+++ src/util/GeradorUtil.java
@@ -10,15 +10,38 @@ package util;
  * @author gabriel.oening
  */
 public class GeradorUtil {
-  
+    
+  /**
+   * Retorna um valor String conforme a quantidade de número solicitado
+   *  
+   */
+    public String gerarNumero(int qtde){
+        String senha = "";
+        int numero;
+        for(int i = 0; i < qtde; i++){
+            numero = (int)(Math.random() * 10 );
+            senha = senha + numero; 
+        }
+        return senha;
+        
+    }
+    
+    public String gerarCpf(){
+       
+        return gerarNumero(3) + "." + gerarNumero(3) + "." + gerarNumero(3) + "-" + gerarNumero(2);
+    }
+    
+    
   public double gerarSalario(){
        int numero = (int) (Math.random() * 100000);
       return numero;
-  } 
-  
+  }
+   
     public static void main(String[] args) {
-       GeradorUtil objetoDessaClasse = new GeradorUtil();
-       System.out.println("salário: " + objetoDessaClasse.gerarSalario());
-    
+       GeradorUtil util = new GeradorUtil();
+       String cpf = util.gerarCpf();
+       System.out.println("(CPF: " + cpf);
+       String numero = util.gerarNumero(4);
+       System.out.println("Senha " + numero);
     }
 }
